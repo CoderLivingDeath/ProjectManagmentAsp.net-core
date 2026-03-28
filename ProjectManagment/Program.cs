384f9ce1828b6d53299a331e273f3778a7d5c226
Eugene Nalivkin <CoderLivingDeath@gmail.com>
2026-03-28 19:43:43 +0700

    feat: add ASP.NET Core MVC with session support

--- ProjectManagment/Program.cs
+++ ProjectManagment/Program.cs
@@ -12,7 +12,11 @@ namespace ProjectManagment
 
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            ConfigureMiddleware(app);
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
@@ -20,7 +24,35 @@ namespace ProjectManagment
         private static void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AppDbContext>();
+            services.AddControllersWithViews();
+
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+                options.Cookie.Name = ".ProjectManagment.Session";
+            });
         }
 
+        private static void ConfigureMiddleware(WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseRouting();
+        }
     }
 }
